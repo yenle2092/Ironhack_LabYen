fd33a4346bcceba7e33b1bdeda96d502ee9991b9
YEN LE <xumi2092@gmail.com>
2023-01-30 23:27:05 +0100

    Refactoring

--- Module_1/Lab_14_Refactoring/Game_Guess_the_number_original.py
+++ Module_1/Lab_14_Refactoring/Game_Guess_the_number_original.py
@@ -4,49 +4,37 @@
 # In[ ]:
 
 
-### CHOOSE THE LEVEL OF THE GAME
+###Choose the level of the game
 
 list_level=["1","2","3"]
 print("                 WELCOME TO THE GAME: GUESS THE NUMBER 😊😊😊") 
 print("\nEntrer 1 for Easy level \nEntrer 2 for Medium level \nEntrer 3 for Hard level")
 level=input("\nChoose the level of the game: ")
-
-### Make sure the correct input
-
 while level not in list_level:
     level=input("\nThe level is not correct, choose only the number 1,2 or 3 : ")
-
-## LEVEL 1: EASY LEVEL
-
+    
 if level=="1":
     print("\n           YOU CHOSE THE EASY LEVEL \n               LET'S GO - Just 5 attempts")
     x=1
     y=10
     trial=5
-    
-## LEVEL 2: MEDIUM LEVEL
-
 elif level=="2":
     print("\n           YOU CHOSE THE MEDIUM LEVEL \n              LET'S GO - Just 7 attempts")
     x=10
     y=100 
     trial=7
-    
-## LEVEL 3: HARD LEVEL
-
 elif level=="3":
     print("\n           YOU CHOSE THE HARD LEVEL \n                 LET'S GO - Just 10 attempts")
     x=-500
     y=500
     trial=10
     
-### CHOOSE A RANDOM NUMBER
+###Choose a random number (guess)
 import random
 #print(x,y)
 number=random.randint(x,y)  
 
-### GUESS THE NUMBER 
-
+###Guess a number:
 count=0
 while count < trial:
     try:
@@ -65,6 +53,7 @@ while count < trial:
         print("\n       Invalid input! Please enter a number.")
 else:
     print("\n             YOU LOST THE GAME \nYou have reached the maximum number of attempts. \nThe number was", number)
+
 
 
 # In[ ]:
