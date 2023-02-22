f23f69b03a361e308743e61f52d97d02ef20f8e7
YEN LE <xumi2092@gmail.com>
2023-02-22 23:30:24 +0100

    add lab 22 completed

---
+++ Module_1/Lab_17_Understanding-Code/README.md
@@ -0,0 +1,27 @@
+![logo_ironhack_blue 7](https://user-images.githubusercontent.com/23629340/40541063-a07a0a8a-601a-11e8-91b5-2f13e4e6b441.png)
+# Lab | Understanding others code
+
+## Description
+Yesterday we spoke about your first week's project. Today we ask you to take someone else's code, understand it and improve it. 
+
+## Goals
+This is important because you:
+* You learn how to read and understand the code.
+* Learn from others by reading their projects.
+  * New methods.
+  * New ways of thinking.
+  * Best practices.
+* Your partner learns from you thanks to your feedback.
+  * He/she knows his/her strenghts.
+  * He/she learns some ways to improve.
+
+## Instructions
+* Look for the repository of the partner you have been assigned to (TA will share in the Slack channel).
+* First, understand the workflow he/she built in their improved version of their code.
+* Draw / write the workflow for yourself and note your doubts about it.
+* Look for things he/she made that you think could be useful for you.
+* What should be improved?
+* Fork the code
+* Make the improvements
+* Commit the improvements
+* Comment your improvements
--- Module_1/Lab_22_Advanced-MySql/solution.sql
+++ Module_1/Lab_22_Advanced-MySql/solution.sql
@@ -37,7 +37,7 @@ GROUP BY au_id, title_id;
 
 SELECT au_id as "AUTHOR ID",
 	   (sum(prof.royal)+sum(prof.adv)) as PROFIT
-FRom
+FROM
 	(SELECT title_id as "TITLE ID", 
 	   au_id,
        sum(sa.sales_royalty) as royal,
@@ -57,6 +57,52 @@ GROUP BY au_id, title_id ) prof
 group by au_id
 order by  profit desc
 limit 3;
+
+    -- Challenge 2 - Alternative Solution
+    
+CREATE TEMPORARY TABLE tbl1
+SELECT 
+		a.au_id, 
+        t.title_id, (SUM((t.advance * ta.royaltyper / 100)) + SUM((t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100))) AS total
+FROM titleauthor ta
+INNER JOIN authors a
+		   ON ta.au_id = a.au_id
+INNER JOIN titles t
+		   ON ta.title_id = t.title_id
+INNER JOIN sales s
+		   ON t.title_id = s.title_id
+GROUP BY a.au_id, t.title_id
+ORDER BY a.au_id
+;
+
+SELECT 
+		tbl1.au_id, 
+        SUM(tbl1.total) as profit
+FROM tbl1
+GROUP BY tbl1.au_id
+ORDER BY profit DESC
+LIMIT 3
+;
+
+--  Challenge 3: Most profiting author
+
+/* Elevating from your solution in Challenge 1 & 2, create a permanent table named most_profiting_authors to hold the data about the most profiting authors. The table should have 2 columns:
+
+au_id - Author ID
+profits - The profits of the author aggregating the advances and royalties
+*/
+
+-- Challenge 3/ most_profiting_authors
+
+CREATE TABLE most_profiting_authors
+SELECT 
+		tbl1.au_id, 
+        SUM(tbl1.total) as profitS
+FROM tbl1
+GROUP BY tbl1.au_id
+ORDER BY profits DESC
+LIMIT 3
+;
 
 
 
